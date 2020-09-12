2872bac99ac1db152cdfd5f5d9a63c301dd9c1dd
Felix Prinz <felix@prinz.no>
2020-09-12 21:56:38 +0200

    initial commit

---
+++ clock.js
@@ -0,0 +1,27 @@
+const setTime = function () {
+  const clockContainer = document.getElementById("clockContainer");
+  const time = new Date();
+  let minutes = time.getMinutes() - (time.getMinutes() % 5);
+  let hour = time.getHours() % 12;
+  clockContainer.className = "";
+  clockContainer.classList.add("all");
+  if (minutes === 0) {
+    clockContainer.classList.add("oClock");
+  } else if (minutes > 30) {
+    minutes = 60 - minutes;
+    hour += 1;
+    clockContainer.classList.add("to");
+  } else {
+    clockContainer.classList.add("past");
+  }
+  hour = ((hour + 11) % 12) + 1;
+  clockContainer.classList.add("hour" + hour);
+  clockContainer.classList.add("offset" + minutes);
+};
+
+const init = function () {
+  setTime();
+  setInterval(setTime, 1000);
+};
+
+window.onload = setTime;
