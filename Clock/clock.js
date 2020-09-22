5844f5245b99dfc68719a54d579c865a185e9963
Felix Prinz <felix@prinz.no>
2020-09-22 21:53:04 +0200

    added more languages

--- Clock/clock.js
+++ Clock/clock.js
@@ -1,25 +1,59 @@
-const setTime = function () {
-  const clockContainer = document.getElementById("clock");
-  const time = new Date();
-  let minutes = time.getMinutes() - (time.getMinutes() % 5);
-  let hour = time.getHours() % 12;
+const languages = ["De", "No", "Eng"];
+const offsets = {
+  De: 15,
+  No: 15,
+  Eng: 30,
+};
+
+const setTimeForLanguage = function (language, hour, minutes) {
+  const clockContainer = document.getElementById("clock" + language);
+
   clockContainer.className = "";
   clockContainer.classList.add("all");
-  if (minutes === 0) {
-    clockContainer.classList.add("oClock");
-  } else if (minutes > 30) {
-    minutes = 60 - minutes;
-    hour += 1;
-    clockContainer.classList.add("to");
-  } else {
-    clockContainer.classList.add("past");
-  }
-  hour = ((hour + 11) % 12) + 1;
-  clockContainer.classList.add("hour" + hour);
+  clockContainer.classList.add("hour" + (hour + (minutes > offsets[language])));
   clockContainer.classList.add("offset" + minutes);
 };
 
+const setTime = function () {
+  const time = new Date();
+  const minutes = time.getMinutes() - (time.getMinutes() % 5);
+  const hour = ((time.getHours() + 11) % 12) + 1;
+
+  const currentLanguage = document.getElementById("languageSelect").value;
+  setTimeForLanguage(currentLanguage, hour, minutes);
+};
+
+const setLanguage = function (language) {
+  for (const oldLanguage of languages) {
+    document.body.classList.remove("show" + oldLanguage);
+  }
+  document.body.classList.add("show" + language);
+};
+
+const changeLanguage = function (event) {
+  setLanguage(event.target.value);
+  setTime();
+};
+
+const initLanguage = function () {
+  let language = new URLSearchParams(window.location.search).get("language");
+  language =
+    language &&
+    language.charAt(0).toUpperCase() + language.slice(1).toLowerCase();
+  if (!languages.includes(language)) {
+    language = languages[Math.floor(Math.random() * 3)];
+  }
+  document.getElementById("languageSelect").value = language;
+
+  setLanguage(language);
+
+  document
+    .getElementById("languageSelect")
+    .addEventListener("change", changeLanguage);
+};
+
 const init = function () {
+  initLanguage();
   setTime();
   setInterval(setTime, 1000);
 };
